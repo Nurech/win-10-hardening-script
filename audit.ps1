a8fd3aeb8893f24d7c94d6dc03035fd681328071
Joosep Parts <joosep.parts@gmail.com>
2022-11-29 17:43:13 +0200

    Add files via upload

---
+++ audit.ps1
@@ -0,0 +1,11 @@
+﻿$1_1 = Test-Path HKEY_LOCAL_MACHINE\Software\Microsoft\OLE
+
+echo "###############################################################################################################"
+echo ""
+echo "Starting audit"
+echo ""
+echo "###############################################################################################################"
+echo ""
+echo "1   - [no test] Create a restore point."
+echo "1_1 - [$1_1] Block remote commands."
+pause
